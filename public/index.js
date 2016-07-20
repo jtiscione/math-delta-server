c6ff49e027209c34f4d5fd05cc3a1e9e94924d04
Jason Tiscione <tiscione@gmail.com>
2016-07-19 22:02:13 -0600

    added div easter_egg

--- public/index.js
+++ public/index.js
@@ -4,7 +4,8 @@ $(function() {
         var aVal = parseFloat($('#a').val());
         var xVal = parseFloat($('#x').val());
         if (isNaN(aVal) || isNaN(xVal)) {
-            console.log("NaN...");
+            $('#result').text("Aw, snap!");
+            console.log("NaN... a:" + aVal + ", x: " + xVal);
             return;
         }
         $.ajax({
@@ -16,12 +17,18 @@ $(function() {
                 x: xVal
             },
             error: function(jqXHR, textStatus, err) {
+                $('#result').text("Server error.");
+                console.log("NaN... a:" + aVal + ", x: " + xVal);
                 console.log(textStatus+", err "+err);
             }
         }).done(function(data) {
             console.log(data.result);
             var percent = Math.round(1000 * data.result) / 10;
             $('#result').text(percent + "%");
+            if (percent === 96.2) {
+                $('#easter_egg').html("<iframe width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/P7jhoby-UUo?start=92&autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+                setTimeout(function(){$('#easter_egg').html('')}, 12000);
+            }
         });
     };
 
@@ -29,3 +36,32 @@ $(function() {
     $('#x').on("input change", listen);
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var checkResult = function(percent) {
+}
